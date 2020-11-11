0e127cd0c472e9580e22d5fe88d3e524a84c464f
Gus Heptig <gusheptig@gmail.com>
2020-11-11 13:05:15 -0500

    added teams colors to json data

--- client/src/components/MatchupCard/index.js
+++ client/src/components/MatchupCard/index.js
@@ -9,7 +9,22 @@ export default class index extends Component {
       <>
         <ul>
           {teamData.map((team, idx) => (
-            <li key={idx}>
+            <li
+              key={idx}
+              style={{
+                backgroundColor: team.color1,
+                color: "#FFFFFF",
+                boxShadow: `4px 2px 1.5px ${team.color2}`,
+                // border: `2px solid ${team.color3}`,
+                marginTop: "10px",
+                marginLeft: "auto",
+                marginRight: "auto",
+                fontSize: "1em",
+                width: "100%",
+                maxWidth: "71px",
+                padding: "20px",
+              }}
+            >
               {team.teamName} ({team.teamAbv})
             </li>
           ))}
