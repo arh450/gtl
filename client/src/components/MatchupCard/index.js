664fcab0cbbf3ce78113e589f6e3d379c78c355d
Gus Heptig <gusheptig@gmail.com>
2020-11-11 20:52:06 -0500

    reformatted matchup card using table, WIP

--- client/src/components/MatchupCard/index.js
+++ client/src/components/MatchupCard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Col, Row, Button } from "react-bootstrap";
+import { Col, Row, Button, Table } from "react-bootstrap";
 import teamData from "../Data/teamData.json";
 
 import "./style.scss";
@@ -11,8 +11,112 @@ export default class index extends Component {
     return (
       <>
         <Row className="mx-auto ">
-          <Col md={4} className="mx-auto">
-            {teamData.map((team, idx) => (
+          <Col md={5} className="mx-auto">
+            <Table>
+              <thead responsive="md">
+                <tr>
+                  <th style={{ backgroundColor: "#425e6a" }}></th>
+                  <th style={{ backgroundColor: "#425e6a" }}>
+                    <p
+                      style={{
+                        backgroundColor: "#97233F",
+                        boxShadow: `4px 2px 1.5px #000000`,
+                        textAlign: "center",
+                        marginTop: "10px",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        fontSize: "1em",
+                        width: "100%",
+                        maxWidth: "71px",
+                        padding: "20px",
+                      }}
+                    >
+                      ARI
+                    </p>
+                  </th>
+                  <th style={{ backgroundColor: "#425e6a" }}>
+                    <p
+                      className="text-center"
+                      style={{
+                        fontSize: "1em",
+                        marginTop: "10px",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        width: "100%",
+                        maxWidth: "71px",
+                        padding: "15px",
+                      }}
+                    >
+                      @
+                    </p>
+                  </th>
+                  <th style={{ backgroundColor: "#425e6a" }}>
+                    <p
+                      style={{
+                        backgroundColor: "#97233F",
+                        boxShadow: `4px 2px 1.5px #000000`,
+                        textAlign: "center",
+                        marginTop: "10px",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        fontSize: "1em",
+                        width: "100%",
+                        maxWidth: "71px",
+                        padding: "20px",
+                      }}
+                    >
+                      ARI
+                    </p>
+                  </th>
+                  <th style={{ backgroundColor: "#425e6a" }}></th>
+                  <th style={{ backgroundColor: "#fa6e06" }}>
+                    <div
+                      className="d-flex justify-content-center"
+                      style={{
+                        marginBottom: "5px",
+                        width: "100%",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        maxWidth: "71px",
+                        padding: "20px",
+                      }}
+                    >
+                      <Button variant="danger" className="mr-2">
+                        -
+                      </Button>
+                      <Button variant="success">+</Button>
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+            </Table>
+          </Col>
+        </Row>
+
+        {/* <ul className="list-group" id="main">
+          {teamData.map((team, idx) => (
+            <li
+              key={idx}
+              className="list-group-item text-white"
+              style={{
+                backgroundColor: team.color1,
+                boxShadow: `4px 2px 1.5px ${team.color2}`,
+                textAlign: "center",
+                marginTop: "10px",
+                marginLeft: "auto",
+                marginRight: "auto",
+                fontSize: "1em",
+                width: "100%",
+                maxWidth: "71px",
+                padding: "20px",
+              }}
+            >
+              {team.teamAbv}
+            </li>
+          ))}
+        </ul> */}
+
+        {/* {teamData.map((team, idx) => (
               <Row className="bg-success mt-2" key={idx}>
                 <Col md={6}>
                   <p
@@ -72,31 +176,7 @@ export default class index extends Component {
                 </Col>
               </Row>
             ))}
-          </Col>
-        </Row>
-
-        {/* <ul className="list-group" id="main">
-          {teamData.map((team, idx) => (
-            <li
-              key={idx}
-              className="list-group-item text-white"
-              style={{
-                backgroundColor: team.color1,
-                boxShadow: `4px 2px 1.5px ${team.color2}`,
-                textAlign: "center",
-                marginTop: "10px",
-                marginLeft: "auto",
-                marginRight: "auto",
-                fontSize: "1em",
-                width: "100%",
-                maxWidth: "71px",
-                padding: "20px",
-              }}
-            >
-              {team.teamAbv}
-            </li>
-          ))}
-        </ul> */}
+          </Col> */}
       </>
     );
   }
